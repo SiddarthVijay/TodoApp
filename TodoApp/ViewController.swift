d5b8bd4cd144996560e8932e5cf25d5a19a3a7b6
Siddarth Vijay <vsiddarthvijay@gmail.com>
2019-06-10 16:42:08 +0530

    Added a basic Table view controller with a couple of distinct cells that repeat at regular intervals

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CheckListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1000
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
+        
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            if indexPath.row % 5 == 0 {
+                label.text = String(indexPath.row) + ")" + "Take a jog"
+            } else if indexPath.row % 4 == 0 {
+                label.text = String(indexPath.row) + ")" + "Watch a movie"
+            } else if indexPath.row % 3 == 0 {
+                label.text = String(indexPath.row) + ")" + "Code an app"
+            } else if indexPath.row % 2 == 0 {
+                label.text = String(indexPath.row) + ")" + "Walk the dog"
+            } else if indexPath.row % 1 == 0 {
+                label.text = String(indexPath.row) + ")" + "Study design patterns"
+            }
+        }
+        
+        return cell
+    }
 
 }
 
